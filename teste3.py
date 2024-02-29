e6f621b6288fc2ab0a968fddb931170d0cf7c814
Julio Cesar <julio.junior@vtal.com>
2024-02-28 22:56:49 -0300

    Feature: Refactored

--- teste3.py
+++ teste3.py
@@ -1,23 +1,27 @@
 import json
 from datetime import datetime
 
-with open('response2.json', 'r', encoding='utf-8') as f:
-    fjdata = json.load(f)
-    count = 1
-    words_buffer = []
-    for word in fjdata:
-        words_buffer.append(word)
-        with open('teste.srt', 'a', encoding='utf-8') as file:
-            if len(words_buffer) >= 10:
-                file.write(str(count) + '\n')
-                start_time = datetime.utcfromtimestamp(word['start']).strftime('%H:%M:%S,%f')[:-3]
-                end_time = datetime.utcfromtimestamp(word['end']).strftime('%H:%M:%S,%f')[:-3]
-                file.write(start_time + ' --> ' + end_time + '\n')
+def create_srt_from_json(json_file, output_file, buffer_size=10):
+    with open(json_file, 'r', encoding='utf-8') as f:
+        fjdata = json.load(f)
+        count = 1
+        words_buffer = []
+        with open(output_file, 'a', encoding='utf-8') as file:
+            for word in fjdata:
+                words_buffer.append(word)
+                if len(words_buffer) >= buffer_size:
+                    file.write(str(count) + '\n')
+                    start_time = datetime.utcfromtimestamp(words_buffer[0]['start']).strftime('%H:%M:%S,%f')[:-3]
+                    end_time = datetime.utcfromtimestamp(words_buffer[-1]['end']).strftime('%H:%M:%S,%f')[:-3]
+                    file.write(start_time + ' --> ' + end_time + '\n')
 
-                for item in words_buffer:
-                    file.write(str(item["word"] + ' '))
+                    for item in words_buffer:
+                        file.write(str(item["word"] + ' '))
 
-                file.write('\n\n')
-                words_buffer = []
-                count+=1
-    print(words_buffer)
+                    file.write('\n\n')
+                    words_buffer = []
+                    count += 1
+
+if __name__ == "__main__":
+    create_srt_from_json('response2.json', 'teste.srt')
+    print("Arquivo .srt criado com sucesso!")
